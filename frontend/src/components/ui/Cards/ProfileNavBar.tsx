7f7796f222a5a3356ee0d71553d8b3abae5a0aa1
Gabriel leonett <gabriellenett6688@gmail.com>
2026-06-16 13:06:28 -0400

    Cambios estables

--- frontend/src/components/ui/Cards/ProfileNavBar.tsx
+++ frontend/src/components/ui/Cards/ProfileNavBar.tsx
@@ -8,7 +8,7 @@ import { ThemeContext } from '../../../contexts/ThemeContext.tsx';
 import { useAuthStore } from '../../../stores/authStore.tsx';
 import LanguageSelector from '../../shared/LanguageSelector.tsx';
 import useLanguage from '../../../hooks/useLanguage.tsx';
-import CardBase from './cardBase.tsx'
+import CardBase from './CardBase-tem.tsx'
 
 interface ProfileNavBarProps {
     open?: boolean;
