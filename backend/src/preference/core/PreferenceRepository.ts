0c2271b0a1534e2cc81f3e4eb48fd3fbcbe40bae
Gabriel leonett <gabriellenett6688@gmail.com>
2026-06-24 14:52:01 -0400

    fix: limpiar cache de git y aplicar reglas de gitignore

--- backend/src/preference/core/PreferenceRepository.ts
+++
@@ -1,9 +0,0 @@
-import { UserId } from '../../shared/core/value-objects/UserId';
-import { ErrorAbstract } from '../../shared/error-abstract';
-import { Result } from '../../shared/result';
-import { Preference } from './Preference';
-
-export interface PreferenceRepository {
-  getOneById(id: UserId): Promise<Result<Preference, ErrorAbstract>>;
-  save(userConfig: Preference): Promise<Result<Preference, ErrorAbstract>>;
-}
